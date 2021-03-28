0008e54f098b90c700938ab1c31a8830256db8a9
Anderson Rodrigues <anddersonrds@gmail.com>
2021-03-28 18:51:58 -0300

    Add Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <h2>Dev News!</h2>
+import { GetStaticProps } from 'next'
+import { stripe } from 'services/strapi'
+
+import Home, { HomeProps } from 'templates/Home'
+
+export default function Index({ product }: HomeProps) {
+  return <Home product={product} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Ia4IfCf6WPPPJ3YO8oX2q2W')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
 }
