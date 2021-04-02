9299894a58b7b6adeacd9cff94ca1ac74b51aa2e
Anderson Rodrigues <anddersonrds@gmail.com>
2021-04-02 14:49:43 -0300

    Remove priceId to Button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,5 @@
 import * as S from './styles'
 
-type SubscribeButtonProps = {
-  priceId: string
-}
-
-const SubscribeButton = ({ priceId }: SubscribeButtonProps) => (
-  <S.Wrapper type="button">Subscribe now</S.Wrapper>
-)
+const SubscribeButton = () => <S.Wrapper type="button">Subscribe now</S.Wrapper>
 
 export default SubscribeButton
--- src/components/SubscribeButton/test.tsx
+++ src/components/SubscribeButton/test.tsx
@@ -1,17 +1,14 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import SubscribeButton from '.'
 
 describe('<SubscribeButton />', () => {
   it('should render the ...', () => {
-    const { container } = renderWithTheme(<SubscribeButton />)
+    renderWithTheme(<SubscribeButton />)
 
-    // expect(
-    //   screen.getByRole('heading', { name: /Hauhauhaoh/i })
-    // ).toBeInTheDocument()
-
-    // screen.logTestingPlaygroundURL()
-    // expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('button', { name: /subscribe now/i })
+    ).toBeInTheDocument()
   })
 })
