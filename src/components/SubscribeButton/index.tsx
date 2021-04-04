da1f62758ebcf946f555cbb449010a3293a7654d
Anderson Rodrigues <anddersonrds@gmail.com>
2021-04-04 16:34:03 -0300

    Add Unique Customer to Subscriber

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,18 +1,27 @@
 import { signIn, useSession } from 'next-auth/client'
+import { api } from 'services/api'
+import { getStripeJS } from 'services/stripe-js'
 
 import * as S from './styles'
-
-type SubscribeButtonProps = {
-  priceId: string
-}
 
 const SubscribeButton = () => {
   const [session] = useSession()
 
-  const handleSubscribe = () => {
+  const handleSubscribe = async () => {
     if (!session) {
       signIn('github')
       return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+      const stripe = await getStripeJS()
+
+      await stripe?.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
     }
   }
 
